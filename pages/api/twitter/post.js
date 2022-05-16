9fc8e7f03ba2770e339026a972194b196f9da3ec
Riffyraff <82163631+ArifAli999@users.noreply.github.com>
2022-05-16 06:41:27 +0100

    fixes

--- pages/api/twitter/post.js
+++ pages/api/twitter/post.js
@@ -1,33 +1,42 @@
 import Twitter from 'twitter-lite';
 import { getSession } from 'next-auth/react'
 import { getToken } from 'next-auth/jwt';
+var Twit = require('twit')
+
 export default async (req, res) =>{
-  
-    var Twit = require('twit')
+
+   const status = req.body;
  
    
   
     const session = await getSession({ req });
+    
     const token = await getToken({
       req,
       secret: process.env.NEXTAUTH_SECRET
     });
     console.log(token.twitter.accessToken)
   
-    var T = new Twit({
-        consumer_key: process.env.TWITTER_CONSUMER_KEY,
-        consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
-        access_token_key: token.twitter.accessToken,
-        access_token_secret: token.twitter.refreshToken
-      });
+    const client = new Twitter({
+      subdomain: 'api',
+      consumer_key: process.env.TWITTER_CONSUMER_KEY,
+      consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+      access_token_key: token.twitter.accessToken,
+      access_token_secret: token.twitter.refreshToken
+    });
+  
       
      
-      const body = req.body;
-      T.post('statuses/update', { status: req.body }, function(err, data, response) {
-        console.log(data)
-      })
-
-   
-
-    return res.status(200).json({ status: 'ok', body: body});
+    try {
+      const results = await client.post('statuses/update', {
+        status
+      });
+      return res.status(200).json({
+        status: 'Ok'
+      });
+    } catch(e) {
+      return res.status(400).json({
+        status: e.message
+      });
+    }
   }
